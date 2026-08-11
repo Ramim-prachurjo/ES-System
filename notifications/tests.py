122083647c36eeb11219d74eeb43cd03abba33de
naba611 <samianaba2003@gmail.com>
2026-08-11 22:59:09 +0600

    Update tests.py

--- notifications/tests.py
+++ notifications/tests.py
@@ -6,3 +6,33 @@ from accounts.models import CustomUser
 from .models import Notification
 
 
+class NotificationModelTest(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="testuser",
+            password="testpass123",
+            role="player",
+        )
+
+    def test_notification_creation(self):
+        notification = Notification.objects.create(
+            user=self.user,
+            message="You were approved!",
+        )
+
+        self.assertEqual(
+            notification.user,
+            self.user
+        )
+
+        self.assertEqual(
+            notification.message,
+            "You were approved!"
+        )
+
+        self.assertFalse(
+            notification.is_read
+        )
+
+
